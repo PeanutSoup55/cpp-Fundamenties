9d9c36b91fd7a1cf831b41be43d83cf1234e5e32
PeanutSoup55 <ericblanchette55hockey@gmail.com>
2026-07-29 15:26:53 -0400

    made function

--- func.cpp
+++ func.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+//func that computes sales tax on the subtotal
+double computeTax(double subTotal);
 
+const double SALES_TAX = 0.075;
+
+
+int main(){
+    double tax = 10.0;
+    double total = tax + computeTax(tax);
+
+    cout << total << endl;
     return 0;
 }
+
+double computeTax(double tax){
+    return tax * SALES_TAX;
+}
+
